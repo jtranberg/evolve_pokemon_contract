daf5505ff86b4020ffee6b059be4455f26b11193
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-12-22 11:52:35 -0800

    commit

---
+++ src/Contract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
+
+contract PokemonEvolveContract is ERC1155LazyMint {
+    constructor(
+        string memory _name,
+        string memory _symbol
+
+    ) ERC1155LazyMint(_name, _symbol, msg.sender, 0) {}
+
+        function verifyClaim(
+        address _claimer,
+        uint256 _tokenId,
+        uint256 _quantity
+    ) public view override {
+    require(_tokenId == 0, "Only Squirtle is claimable");
+    require(_quantity == 1, "Only 1 Squirtle can be clamed");
+    }
+
+    function evolve() public {
+        _burn(msg.sender, 0, 2);
+        _mint(msg.sender, 1, 1, "");
+    }
+}
